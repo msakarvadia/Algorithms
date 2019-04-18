d10913c391905732132d4761b69ad7948348eae9
msakarvadia <mansi.sakarvadia@gmail.com>
2019-04-18 00:33:34 -0400

    lomuto and hoare algorithms finished

--- quicksort.c
+++ quicksort.c
@@ -14,20 +14,42 @@
 //for hore pivot should still be a midian of three value, and u should place that median in the middle of array - see wiki "choice of pivot" snippet for pointer
 
 size_t lomuto_partition(int *arr, int l, int h);
+size_t hoare_partition(int *arr, int l, int h);
 void quickSort_lomuto(int *arr, int l, int h);
+void quickSort_hoare(int *arr, int l, int h);
 int medianOfthree(int *arr, int l, int h);
 void print(int *arr);
 
 int main(void){
+    clock_t t;
+    printf("Haore's partitioning does fewer swaps on average, so should be more efficient and faster in practice.");
     int arr[SIZE];
+    int duplicate[SIZE];
     for(size_t i = 0; i < SIZE; ++i){
         arr[i] = rand() % SIZE + 1;
+        duplicate[i] = arr[i];
         }  
     printf("Unsorded array: \n");
     print(arr);
+    t = clock();
     quickSort_lomuto(arr, 0, SIZE-1);
-    printf("sorded array: \n");
+    t = clock() -t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("sorded array by lomuto: \n");
     print(arr);
+    printf("time for lomuto sort: %f\n", time_taken);
+
+    printf("Unsorded array: \n");
+    print(arr);
+    double m = clock();
+    quickSort_hoare(duplicate, 0, SIZE-1);
+    printf("sorded array by hoare: \n");
+    m = clock() -m;
+    double time_taken_two = ((double)m)/CLOCKS_PER_SEC;
+    printf("sorded array by lomuto: \n");
+    print(arr);
+    printf("time for hoare sort: %f\n", time_taken_two);
+    printf("time is so short for sorts that it is hard to measure.");
     return 0;
 }
 
@@ -38,6 +60,44 @@ void print(int *arr){
     puts(" ");
     
     }
+
+void quickSort_hoare(int *arr, int l, int h){
+    if(l<h){
+       size_t p =hoare_partition(arr, l, h);
+//       printf("partition index: %zu, arr[p]: %zu\n", p, arr[p]);
+  //     print(arr);
+       quickSort_hoare(arr, l, p);
+       quickSort_hoare(arr, p+1, h);
+    }
+}
+
+size_t hoare_partition(int *arr, int l, int h){
+    int pivot_index = medianOfthree(arr, l, h);
+    int first_tmp = arr[l];
+    arr[l] = arr[pivot_index];
+    arr[pivot_index] = first_tmp;
+  
+    int i = l -1;
+    int j = h +1;
+
+    while(true){
+        i++;
+        while(arr[i] < arr[l]){
+            i++;
+        }
+        j--;
+        while(arr[j] > arr[l]){
+            j--;
+        }                   
+        if (i >= j){
+            return j;
+        }
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+
+    }    
+}
 
 void quickSort_lomuto(int *arr, int l, int h){
     if(l<h){
