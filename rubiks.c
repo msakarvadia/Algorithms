595c00b29adc32af501b9ae4b551d96072ef18fb
msakarvadia <mansi.sakarvadia@gmail.com>
2019-04-24 22:16:58 -0400

    all moves should be working but need testing

--- rubiks.c
+++ rubiks.c
@@ -13,6 +13,7 @@ void f(int** cube);
 void b(int** cube);
 void l(int** cube);
 void r(int** cube);
+void swap(int** cube, int r1, int c1, int r2, int c2);
 
 int main(void){
     int** cube = makeCube();
@@ -58,8 +59,9 @@ int main(void){
                 }
             }
         }
-
-        return 0;
+    u(cube);
+    printCube(cube);
+    return 0;
     }
 }
 
@@ -80,3 +82,129 @@ void printCube(int** cube){
     }
     puts(" ");
 }
+
+void swap(int** cube, int r1, int c1, int r2, int c2){
+    int tmp = cube[r1][c1];
+    cube[r1][c1] = cube[r2][c2];
+    cube[r2][c2] = tmp;
+}
+
+//"up" - top layer twisted clock-wise
+void u(int** cube){
+    swap(cube, 3,4,4,5);
+    swap(cube, 3,4,5,4);
+    swap(cube, 3,4,4,3);
+    swap(cube, 3,3,3,5);
+    swap(cube, 3,3,5,5);
+    swap(cube, 3,3,5,3);
+    swap(cube, 2,4,4,6);
+    swap(cube, 2,4,6,4);
+    swap(cube, 2,4,4,2);
+    swap(cube, 2,3,3,6);
+    swap(cube, 2,3,6,5);
+    swap(cube, 2,3,5,2);
+    swap(cube, 2,5,5,6);
+    swap(cube, 2,5,6,3);
+    swap(cube, 2,5,3,2);
+    printf("Step: U");
+}
+
+//"down" - botton layer twisted clock-wise
+void d(int** cube){
+    swap(cube,3,10,4,11);
+    swap(cube,3,10,5,10);
+    swap(cube,3,10,4,9);
+    swap(cube,3,9,3,11);
+    swap(cube,3,9, 5,11);
+    swap(cube,3,9, 4,11);
+    swap(cube,0,5,3,0);
+    swap(cube,0,5,8,3);
+    swap(cube,0,5,5,8);
+    swap(cube,0,4,4,0);
+    swap(cube,0,4,8,4);
+    swap(cube,0,4,4,8);
+    swap(cube,0,3,5,0);
+    swap(cube,0,3,8,5);
+    swap(cube,0,3,3,8);
+    printf("Step: D");
+    }
+
+//"right" - turn right side towards you
+void r(int** cube){
+    swap(cube, 3,7,4,6);
+    swap(cube, 3,7,5,7);
+    swap(cube, 3,7,4,8);
+    swap(cube, 3,8,3,6);
+    swap(cube, 3,8,5,6);
+    swap(cube, 3,8,5,8);
+    swap(cube, 0,5,3,5);
+    swap(cube, 0,5,6,5);
+    swap(cube, 0,5,5,9);
+    swap(cube, 1,5,4,5);
+    swap(cube, 1,5,7,5);
+    swap(cube, 1,5,4,9);
+    swap(cube, 2,5,5,5);
+    swap(cube, 2,5,8,5);
+    swap(cube, 2,5,3,9);
+    printf("Step: R");
+    }
+
+//"left" - turn left side towards you    
+void l(int** cube){
+    swap(cube, 3,1,4,2);
+    swap(cube, 3,1,5,1);
+    swap(cube, 3,1,4,2);
+    swap(cube, 3,0,3,2);
+    swap(cube, 3,0,5,2);
+    swap(cube, 3,0,5,0);
+    swap(cube, 0,3,3,3);
+    swap(cube, 0,3,6,3);
+    swap(cube, 0,3,5,11);
+    swap(cube, 1,3,4,3);
+    swap(cube, 1,3,7,3);
+    swap(cube, 1,3,4,11);
+    swap(cube, 2,3,5,3);
+    swap(cube, 2,3,8,3);
+    swap(cube, 2,3,3,11);
+    printf("Step: L");
+    }
+
+//"front" - twist front face clockwise
+void f(int** cube){
+    swap(cube,6,4,7,3);
+    swap(cube,6,4,8,4);
+    swap(cube,6,4,7,5);
+    swap(cube,6,5,6,3);
+    swap(cube,6,5,8,3);
+    swap(cube,6,5,8,5);
+    swap(cube,5,5,5,2);
+    swap(cube,5,5,0,3);
+    swap(cube,5,5,5,8);
+    swap(cube,5,4,5,1);
+    swap(cube,5,4,0,4);
+    swap(cube,5,4,5,7);
+    swap(cube,5,3,5,0);
+    swap(cube,5,3,0,5);
+    swap(cube,5,3,5,6);
+    printf("Step: F");
+    }
+
+//"back" - twist back face clockwise -relative to the human's perception of clockwise
+void b(int** cube){
+    swap(cube, 0,3,0,5);
+    swap(cube, 0,3,2,5);
+    swap(cube, 0,3,2,3);
+    swap(cube, 0,4,1,5);
+    swap(cube, 0,4,2,4);
+    swap(cube, 0,4,1,3);
+    swap(cube, 8,3,3,8);
+    swap(cube, 8,3,3,5);
+    swap(cube, 8,3,3,2);
+    swap(cube, 8,4,3,7);
+    swap(cube, 8,4,3,4);
+    swap(cube, 8,4,3,1);
+    swap(cube, 8,5,3,6);
+    swap(cube, 8,5,3,3);
+    swap(cube, 8,5,3,0);
+    printf("Step: B");
+    }
